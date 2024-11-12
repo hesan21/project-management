1ef32ca8e9599e7ad30d9849f017d780e0b6b9f3
hesan21 <hesannaveed21@gmail.com>
2024-11-12 17:47:04 +0500

    Changes for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '/project-management',
+    plugins: [react()],
+    build: {
+        outDir: 'build',  // Set output directory to 'build'
+        assetsDir: 'assets'
+    }
 })
